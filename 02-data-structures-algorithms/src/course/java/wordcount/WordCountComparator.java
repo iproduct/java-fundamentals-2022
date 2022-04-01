d12f261dc62a94f4c3aff35d928f7d3f9e8e54bc
Trayan Iliev <office@iproduct.org>
2022-04-01 12:48:01 +0300

    WordCount demo - stop words added

--- 02-data-structures-algorithms/src/course/java/wordcount/WordCountComparator.java
+++ 02-data-structures-algorithms/src/course/java/wordcount/WordCountComparator.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class WordCountComparator implements Comparator<Map .Entry<String, Integer>> {
     @Override
     public int compare(Map.Entry<String, Integer> wc1, Map.Entry<String, Integer> wc2) {
-        var countsCompare =  wc1.getValue().compareTo(wc2.getValue());
+        var countsCompare =  -wc1.getValue().compareTo(wc2.getValue());
         return countsCompare == 0? wc1.getKey().compareToIgnoreCase(wc2.getKey()): countsCompare;
     }
 }
