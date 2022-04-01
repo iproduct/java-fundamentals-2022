58d07a82bb5065000e70fda2c4c072d73c88274e
Trayan Iliev <office@iproduct.org>
2022-04-01 10:21:13 +0300

    array sums demo

--- 02-data-structures-algorithms/src/course/java/arrays/MultiDimArray.java
+++ 02-data-structures-algorithms/src/course/java/arrays/MultiDimArray.java
@@ -7,22 +7,43 @@ public class MultiDimArray {
     public static final int NUM_MONTHS = 12;
     private static Random rand = new Random();
 
-    public static String formatAsTable(int[][] data){
+
+    public static String formatAsTable(int[][] data) {
         return ""; // TODO Your code here
     }
 
-    public static String formatAsTableWithTotals(int[][] data){
-        return ""; // TODO Your code here
+    public static String formatAsTableWithTotals(int[][] data) {
+        StringBuilder sb = new StringBuilder();
+        int grandTotal = 0;
+        int rowSum = 0;
+        int[] columnSums = new int[NUM_MONTHS];
+        for (int i = 0; i < data.length; ++i) {
+            for (int j = 0; j < data[i].length; ++j) {
+                rowSum += data[i][j];
+                columnSums[j] += data[i][j];
+                grandTotal += rowSum;
+                sb.append(String.format("| %4d ", data[i][j]));
+            }
+            sb.append("| = ").append(rowSum).append("\n");
+
+            rowSum = 0;
+        }
+
+        for (int i = 0; i < NUM_MONTHS; ++i) {
+            sb.append(String.format("  %4d ", columnSums[i]));
+        }
+        sb.append(String.format("  %4d ", grandTotal));
+        return sb.toString();
     }
 
     public static void main(String[] args) {
         int[][] sales = new int[NUM_BOOKS][NUM_MONTHS];
-        for(int i = 0; i < sales.length; i++) {
-            for(int j = 0; j < sales[i].length; j++) {
+        for (int i = 0; i < sales.length; i++) {
+            for (int j = 0; j < sales[i].length; j++) {
                 sales[i][j] = rand.nextInt(900) + 300;
             }
         }
-        System.out.println(formatAsTable(sales));
+        System.out.println(formatAsTableWithTotals(sales));
     }
 }
 
