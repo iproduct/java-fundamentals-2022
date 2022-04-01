c933b205e8e9c59c6262f7c2578350add34d992a
Trayan Iliev <office@iproduct.org>
2022-04-01 13:00:11 +0300

    models and repos added from previous project

---
+++ 02-data-structures-algorithms/src/course/java/model/Person.java
@@ -0,0 +1,89 @@
+package course.java.model;
+
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(Long id, String firstName, String lastName, int age) {
+        this(firstName, lastName, age);
+        this.age = age;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        return getId() != null ? getId().equals(person.getId()) : person.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Person{");
+        sb.append("id=").append(id);
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", age=").append(age);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public String format() {
+        return String.format("| %3.3s | %-15.15s | %-15.15s | %-3d |", id, firstName, lastName, age);
+    }
+
+    public String format(String format) {
+        return String.format(format, id, firstName, lastName, age);
+    }
+}
