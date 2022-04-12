34ea5da478321a22cecdda76d805c7e5fb804bd7
Trayan Iliev <office@iproduct.org>
2022-04-12 09:38:41 +0300

    UserController added

---
+++ 03-oop-inheritance-composition/src/course/java/controller/UserController.java
@@ -0,0 +1,43 @@
+package course.java.controller;
+
+
+import course.java.model.User;
+import course.java.service.UserService;
+import course.java.view.EntityDialog;
+import course.java.view.Menu;
+
+import java.util.List;
+
+public class UserController {
+    private UserService userService;
+    private EntityDialog<User> addUserDialog;
+
+    public UserController(UserService userService, EntityDialog<User> addUserDialog) {
+        this.userService = userService;
+        this.addUserDialog = addUserDialog;
+    }
+
+    public void init() {
+//        userService.loadData();
+        var menu = new Menu("Users Menu", List.of(
+                new Menu.Option("Load Users", () -> {
+                    System.out.println("Loading users ...");
+                    userService.loadData();
+                    return "Users loaded successfully.";
+                }),
+                new Menu.Option("Print All Users", () -> {
+                    var users = userService.getAllUsers();
+                    users.forEach(user -> System.out.println(user.format()));
+                    return "Total user count: " + users.size();
+                }),
+                new Menu.Option("Add New User", () -> {
+                    var user = addUserDialog.input();
+                    var created = userService.addUser(user);
+                    return String.format("User ID:%s: '%s' added successfully.",
+                            created.getId(), created.getUsername());
+                })
+        ));
+        menu.show();
+    }
+
+}
