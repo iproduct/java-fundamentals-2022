d45cfdff9c363accd0548d99c8b098492a956a01
Trayan Iliev <office@iproduct.org>
2022-04-18 11:03:49 +0300

    Bank transfer simulation - problem: no mutex

--- 06-multithreading/src/main/java/course/java/mutithreading/synchronization/Bank.java
+++ 06-multithreading/src/main/java/course/java/mutithreading/synchronization/Bank.java
@@ -1,16 +1,25 @@
 package course.java.mutithreading.synchronization;
 
+import course.java.util.Tuple;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 class Client implements Runnable {
     private long id;
     private Bank bank;
+    private Account from, to;
     private Random rand = new Random();
     private volatile boolean canceled;
 
-    public Client(long id, Bank bank) {
+    public Client(long id, Bank bank, Account from, Account to) {
         this.id = id;
         this.bank = bank;
+        this.from = from;
+        this.to = to;
     }
 
     public void cancel() {
@@ -21,7 +30,7 @@ class Client implements Runnable {
     public void run() {
         while(!canceled && !Thread.interrupted()) {
             bank.transferAmountInCents(rand.nextInt(100) + 1L,
-                    bank.getAccountA(), bank.getAccountB());
+                    from, to);
             try {
                 Thread.sleep(rand.nextInt(100));
             } catch (InterruptedException e) {
@@ -46,7 +55,11 @@ public class Bank {
     }
 
     public boolean transferAmountInCents(long amount, Account from, Account to) {
-        if(from.getBallanceInCents() < amount) return false; // not enough money
+        if (from.getBallanceInCents() < amount) {
+            System.out.printf("Insufficient ballance in account %s : %d cents%n",
+                    from.getId(), from.getBallanceInCents());
+            return false; // not enough money
+        }
         from.setBallanceInCents(from.getBallanceInCents() - amount);
         to.setBallanceInCents(to.getBallanceInCents() + amount);
         System.out.printf("Transfer from %s to %s: %8.2f Euro%n",
@@ -59,7 +72,40 @@ public class Bank {
                 ACCOUNT_A_AMOUNT + ACCOUNT_B_AMOUNT;
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        var bank = new Bank();
+        var executor = Executors.newCachedThreadPool();
+        List<Tuple<Client, Future<?>>> clientData = new ArrayList<>();
+        for (long i = 0; i < 5; i++) {
+            var clientTask = new Client(i, bank, bank.getAccountA(), bank.getAccountB());
+            var future = executor.submit(clientTask);
+            clientData.add(new Tuple<>(clientTask, future));
+        }
+        for (long i = 10; i < 15; i++) {
+            var clientTask = new Client(i, bank, bank.getAccountB(), bank.getAccountA());
+            var future = executor.submit(clientTask);
+            clientData.add(new Tuple<>(clientTask, future));
+        }
+        var checkerFuture = executor.submit(() -> {
+            while (!Thread.interrupted()) {
+                if (!bank.checkAmountsTotal()) {
+                    System.out.printf("PANIC: Constant money constraint violated: should be: %d cents, actual %d cents%n",
+                            ACCOUNT_A_AMOUNT + ACCOUNT_B_AMOUNT,
+                            bank.getAccountA().getBallanceInCents() + bank.getAccountA().getBallanceInCents());
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    System.out.println("Checker thread is interrupted - finishing ...");
+                }
+            }
+        });
+        Thread.sleep(60000);
+        for (int i = 0; i < clientData.size(); i++) {
+            clientData.get(i).getV2().cancel(true);
+            System.out.printf("Client %d canceled.", clientData.get(i).getV1());
+        }
+        checkerFuture.cancel(true);
+        executor.shutdown();
     }
 }
