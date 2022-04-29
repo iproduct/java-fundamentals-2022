38b111ec6ac267dd5753e834fd0054d6a5454479
Trayan Iliev <office@iproduct.org>
2022-04-29 09:05:25 +0300

    presentation updated

--- 12-junit4/src/main/java/course/java/simple/Calculator.java
+++ 12-junit4/src/main/java/course/java/simple/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public int add(int x, int y) {
         return x + y;
     }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
 }
--- 12-junit4/src/test/java/course/java/simple/CalculatorTest.java
+++ 12-junit4/src/test/java/course/java/simple/CalculatorTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 @Slf4j
 public class CalculatorTest {
@@ -38,5 +39,38 @@ public class CalculatorTest {
         int result = calculator.add(5, 8);
         // verify
         assertEquals("Addition should work", 13, result);
+    }
+
+    @Test
+    public void givenXandY_whenDivide_thenDivisionResultTimesYinX() {
+        log.info("Testing divide");
+        // test
+        int result = calculator.divide(23, 5);
+        // verify
+        assertEquals("Regular division should work", 4, result);
+    }
+
+    @Test
+    public void givenXandNegativeY_whenDivide_thenDivisionResultTimesYinX() {
+        log.info("Testing divide ny negative");
+        // test
+        int result = calculator.divide(23, -5);
+        // verify
+        assertEquals("Regular division should work", -4, result);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void givenXandZero_whenDivide_thenArtithmeticException() {
+        log.info("Testing divide by zero");
+        // test
+        int result = calculator.divide(23, 0);
+    }
+
+    @Test
+    public void givenXandZero_whenDivide_thenThrowsArtithmeticException() {
+        log.info("Testing divide by zero - assertThrowing()");
+        // verify Exception thrown
+        assertThrows("Should throw ArtithmenticException", ArithmeticException.class,
+                () -> calculator.divide(23, 0));
     }
 }
