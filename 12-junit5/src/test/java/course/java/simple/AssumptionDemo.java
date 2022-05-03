053ef781d12bfad7fea9d9acb36dffa2386fbd67
Trayan Iliev <office@iproduct.org>
2022-05-03 09:29:54 +0300

    JUnit 5 assumptions demo added

---
+++ 12-junit5/src/test/java/course/java/simple/AssumptionDemo.java
@@ -0,0 +1,34 @@
+package course.java.simple;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+public class AssumptionDemo {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void testOnlyInCIServer() {
+        assumeTrue("CI".equals(System.getenv("ENV")));
+        assertEquals(42, calculator.multiply(6, 7));
+    }
+    @Test
+    void testOnlyOnDeveloperWorkstation() {
+        assumeTrue("DEV".equals(System.getenv("ENV")));
+        assertEquals(BigInteger.valueOf(1000000007), calculator.generateNextPrime(BigInteger.valueOf(1000000000)));
+    }
+    @Test
+    void testInAllEnvironments() {
+        assumingThat("CI".equals(System.getenv("ENV")),
+                () -> assertEquals(42, calculator.multiply(6, 7)));
+
+        assumingThat("DEV".equals(System.getenv("ENV")),
+                () -> assertEquals(BigInteger.valueOf(1000000007), calculator.generateNextPrime(BigInteger.valueOf(1000000000))));
+    }
+
+
+}
