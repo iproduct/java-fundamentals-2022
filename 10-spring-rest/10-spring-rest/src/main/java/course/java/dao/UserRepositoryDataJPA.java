ff2f46855bdcad3a971fed8a0c221217dfdb8b4a
Trayan Iliev <office@iproduct.org>
2022-05-05 11:50:57 +0300

    User entity JPA configured

---
+++ 10-spring-rest/10-spring-rest/src/main/java/course/java/dao/UserRepositoryDataJPA.java
@@ -0,0 +1,10 @@
+package course.java.dao;
+
+import course.java.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepositoryDataJPA extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String user);
+}
