0d196aa01a419387b304210047b01d0903b8fcb7
Trayan Iliev <office@iproduct.org>
2022-05-10 09:19:11 +0300

    Repository

---
+++ 13-spring-core/src/main/java/course/java/dao/Repository.java
@@ -0,0 +1,15 @@
+package course.java.dao;
+
+import course.java.model.Identifiable;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface Repository<K, V extends Identifiable<K>> {
+    Collection<V> findAll();
+    Optional<V> findById(K id);
+    V create(V entity);
+    Optional<V> update(V entity);
+    Optional<V> deleteById(K id);
+    long count();
+}
