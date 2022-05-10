8938d6ced9de7386ce00e614494a46b14847d5ac
Trayan Iliev <office@iproduct.org>
2022-05-10 10:06:11 +0300

    ArticleProviderDefaultImpl added with default articles

--- 13-spring-core/src/main/java/course/java/service/impl/ArticleProviderDefaultImpl.java
+++ 13-spring-core/src/main/java/course/java/service/impl/ArticleProviderDefaultImpl.java
@@ -8,9 +8,18 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class ArticleProviderDefaultImpl implements ArticleProvider {
+    public static final List<Article> DEFAULT_ARTICLES = List.of(
+ new Article("Spring Data JPA Intro", "Spring Data JPA is easy ...",
+                    "Trayan Iliev", Set.of("spring", "mvc", "boot", "intro")),
+            new Article("Spring Data JPA and Hibernate", "Hibernate provides powerful ORM implementation ...",
+                    "Georgi Petrov", Set.of("hibernate", "performance")),
+            new Article( "Spring Data is Going Reactive", "Spring Data provides reactive db integrations for a number of databases ...",
+                    "Trayan Iliev", Set.of("spring", "boot", "intro"))
+    );
     private ArticleRepository articleRepo;
 
     @Autowired
