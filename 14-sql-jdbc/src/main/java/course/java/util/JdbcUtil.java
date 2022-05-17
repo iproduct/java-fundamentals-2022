70e23e4160bf5bbc029db7b7af6c81f82a8db3e6
Trayan Iliev <office@iproduct.org>
2022-05-17 11:22:07 +0300

    getting entity props by reflection

--- 14-sql-jdbc/src/main/java/course/java/util/JdbcUtil.java
+++ 14-sql-jdbc/src/main/java/course/java/util/JdbcUtil.java
@@ -1,7 +1,10 @@
 package course.java.util;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,7 +12,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class JdbcUtil {
-    public static <T> List<T> getEntities(ResultSet rs, Class<T> entityClass) throws SQLException {
+    public static <T> List<T> getEntities(ResultSet rs, Class<T> entityClass) throws SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         List<T> entities = new ArrayList<>();
         var methods = entityClass.getMethods();
         var propMethodMap = Arrays.stream(methods).filter(method -> method.getName().startsWith("set"))
@@ -20,9 +23,41 @@ public class JdbcUtil {
                     return propName;
                 }, Function.identity()));
 
-
         while(rs.next()) {
-//            entities.add()
+            T entity = entityClass.getDeclaredConstructor(new Class[]{}).newInstance();
+            for(String prop : propMethodMap.keySet()) {
+                var setter = propMethodMap.get(prop);
+                var propClass = setter.getParameterTypes()[0];
+                Object value = null;
+                if(propClass.isEnum()) {
+                    var valueStr = rs.getString(prop);
+                    try {
+                        var valuesMethod = propClass.getDeclaredMethod("valueOf");
+                        Object result = valuesMethod.invoke(new Object[] {propClass, valueStr});
+                        value = propClass.cast(result);
+                    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                switch(propClass.getSimpleName()){
+                    case "Long":
+                    case "long": value = rs.getObject(prop, Long.class); break;
+                    case "Integer":
+                    case "int": value = rs.getInt(prop); break;
+                    case "Double":
+                    case "double": value = rs.getDouble(prop); break;
+                    case "Boolean":
+                    case "boolean": value = rs.getBoolean(prop); break;
+                    case "String": value = rs.getString(prop); break;
+                    case "Date": value = rs.getDate(prop); break;
+                    case "LocalDate": value = rs.getDate(prop).toLocalDate(); break;
+                    case "LocalDateTime": value = LocalDateTime.ofInstant(rs.getDate(prop).toInstant(), ZoneId.systemDefault()); break;
+                }
+                System.out.println(value);
+                setter.invoke(entity, value);
+            }
+            entities.add(entity);
+            System.out.println(entity);
         }
         return entities;
     }
