386f35f2a1ebdc50447e13d39e44781b7af2ff32
Trayan Iliev <office@iproduct.org>
2022-05-17 10:00:36 +0300

    JDBC connection demo added

---
+++ 14-sql-jdbc/src/main/java/course/java/jdbc/JdbcSimpleDemo.java
@@ -0,0 +1,43 @@
+package course.java.jdbc;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+@Slf4j
+public class JdbcSimpleDemo {
+    public void run() throws IOException, ClassNotFoundException {
+        // 1. Load DB connection properties
+        Properties props = new Properties();
+        var dbConfigPath = JdbcSimpleDemo.class.getClassLoader().getResource("jdbc.properties")
+                .getPath();
+        props.load(new FileReader(dbConfigPath));
+
+        // 2. Load DB Driver class (optional)
+        try {
+            Class.forName(props.getProperty("driver"));
+        } catch (ClassNotFoundException ex) {
+            log.error("DB driver class not found: ", ex);
+            throw ex;
+        }
+        log.info("DB driver loaded successfully");
+
+        // 3. Create DB Connection and 4. Create Statement
+        try (var con = DriverManager.getConnection(props.getProperty("url"), props)) {
+            log.info("DB connection created successfully to schema: {}", con.getCatalog());
+            // ...
+        } catch (SQLException e) {
+            log.error("Error connecting to DB driver class not found: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        var demo = new JdbcSimpleDemo();
+        demo.run();
+    }
+}
