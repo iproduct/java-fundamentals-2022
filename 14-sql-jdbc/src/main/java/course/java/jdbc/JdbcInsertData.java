90df65e97331156a1bc7d93fce69f6efb79e8dab
Trayan Iliev <office@iproduct.org>
2022-05-17 13:33:21 +0300

    prevent SQL injection using PreparedStatement with params

--- 14-sql-jdbc/src/main/java/course/java/jdbc/JdbcInsertData.java
+++ 14-sql-jdbc/src/main/java/course/java/jdbc/JdbcInsertData.java
@@ -1,10 +1,10 @@
 package course.java.jdbc;
 
+import course.java.model.MockUsers;
 import course.java.model.User;
 import course.java.util.JdbcUtil;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -17,14 +17,18 @@ import java.util.Properties;
 import static course.java.model.Role.ADMIN;
 
 @Slf4j
-public class JdbcSimpleDemo {
+public class JdbcInsertData {
     public static final String SELECT_ALL_USERS = "select * from users;";
     public static final String SELECT_ALL_USERS_IN_ROLE = "select * from users where role = ?;";
+    public static final String INSERT_NEW_USER_USER =
+            "INSERT INTO `users` " +
+                    "(`firstName`, `lastName`, `age`, `phone`, `username`, `password`, `role`, `active`) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
     public void run() throws IOException, ClassNotFoundException {
         // 1. Load DB connection properties
         Properties props = new Properties();
-        var dbConfigPath = JdbcSimpleDemo.class.getClassLoader().getResource("jdbc.properties")
+        var dbConfigPath = JdbcInsertData.class.getClassLoader().getResource("jdbc.properties")
                 .getPath();
         props.load(new FileReader(dbConfigPath));
 
@@ -40,14 +44,33 @@ public class JdbcSimpleDemo {
         // 3. Create DB Connection and
         try (var con = DriverManager.getConnection(props.getProperty("url"), props)) {
             log.info("DB connection created successfully to schema: {}", con.getCatalog());
-            //4. Create PreparedStatement and provide param value
-            try(var stmt = con.prepareStatement(SELECT_ALL_USERS_IN_ROLE)) {
-                stmt.setString(1, ADMIN.name()); // set param with escaping user values to prevent SQL injection!
+            //4. Insert users using PreparedStatement
+            try (var stmt = con.prepareStatement(INSERT_NEW_USER_USER)) {
+                for(User u : MockUsers.MOCK_USERS) {
+                    stmt.setString(1, u.getFirstName());
+                    stmt.setString(2, u.getLastName());
+                    stmt.setInt(3, u.getAge());
+                    stmt.setString(4, u.getPhone());
+                    stmt.setString(5, u.getUsername());
+                    stmt.setString(6, u.getPassword());
+                    stmt.setString(7, u.getRole().name());
+                    stmt.setBoolean(8, u.isActive());
+                    int numRecords = stmt.executeUpdate();
+                    log.info("Successfully INSERTED {} records.", numRecords);
+                }
+            } catch (SQLException e) {
+                log.error("Error executing DB query: ", e);
+                throw new RuntimeException(e);
+            }
+
+            //5. Create PreparedStatement and provide param value
+            try (var stmt = con.prepareStatement(SELECT_ALL_USERS)) {
+//                stmt.setString(1, ADMIN.name()); // set param with escaping user values to prevent SQL injection!
                 ResultSet rs = stmt.executeQuery();
                 List<User> users = JdbcUtil.getEntities(rs, User.class);
                 users.forEach(System.out::println);
-            }catch (SQLException | NoSuchMethodException | InvocationTargetException | InstantiationException |
-                    IllegalAccessException e) {
+            } catch (SQLException | NoSuchMethodException | InvocationTargetException | InstantiationException |
+                     IllegalAccessException e) {
                 log.error("Error executing DB query: ", e);
                 throw new RuntimeException(e);
             }
@@ -58,9 +81,8 @@ public class JdbcSimpleDemo {
     }
 
 
-
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        var demo = new JdbcSimpleDemo();
+        var demo = new JdbcInsertData();
         demo.run();
     }
 }
