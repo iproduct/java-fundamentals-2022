bcd7b704a76c48675cfcf750df83d0891ed757d6
Trayan Iliev <office@iproduct.org>
2022-05-27 09:18:10 +0300

    simple properties to xml demo added

---
+++ 17-xml-jaxb/src/main/java/course/java/properties_xml/PropertiesToXmlDemo.java
@@ -0,0 +1,60 @@
+package course.java.properties_xml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class PropertiesToXmlDemo {
+
+    public static void printProperties(Properties props) {
+//		props.list(System.out);
+        Enumeration<Object> keys = props.keys();
+        while (keys.hasMoreElements()) {
+            String k = (String) keys.nextElement();
+            System.out.println(k + " = " + props.getProperty(k, "not defined"));
+        }
+
+        System.out.println();
+        Set<Map.Entry<Object, Object>> entries = props.entrySet();
+        for (Map.Entry<Object, Object> e : entries) {
+            System.out.println(e.getKey() + " = " + e.getValue());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        URL appConfigPath = PropertiesToXmlDemo.class.getClassLoader()
+                .getResource("properties/app.properties");
+
+        System.out.println("Path: " + appConfigPath);
+
+        Properties appProps = new Properties();
+        appProps.load(new FileInputStream(appConfigPath.getPath()));
+
+        appProps.setProperty("date", LocalDate.now().toString());
+
+        Path configPath;
+        try {
+            configPath = Paths.get(appConfigPath.toURI());
+            Path xmlPath = configPath.resolve("../application.xml").normalize();
+            appProps.setProperty("config.path", xmlPath.toString());
+            printProperties(appProps);
+
+            appProps.storeToXML(new FileOutputStream(xmlPath.toString()),
+                    "application configuration",
+                    Charset.forName("utf-8").toString());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+}
